6f4ca3e657e0ac7053b4dab5ad7721d8f8047896
Zegendary <zxw1992513@qq.com>
2020-01-31 00:06:12 +0800

    finish game 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { AppComponent } from './app.component';
-import { DragableComponent } from './dragable/dragable.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Dragable2Component } from './dragable2/dragable2.component';
-import { Game11Component } from './game11/game11.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+
+import {AppComponent} from './app.component';
+import {DragableComponent} from './dragable/dragable.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Dragable2Component} from './dragable2/dragable2.component';
+import {Game11Component, ImgOverviewDialogComponent} from './game11/game11.component';
+import { ImgComponent } from './game11/img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DragableComponent,
     Dragable2Component,
-    Game11Component
+    Game11Component,
+    ImgComponent,
+    ImgOverviewDialogComponent
+  ],
+  entryComponents: [
+    ImgOverviewDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DragDropModule
+    DragDropModule,
+    MatIconModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {}
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/game11/option.ts
@@ -0,0 +1,9 @@
+export interface Option {
+  id: number;
+  correct: boolean;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  text?: string;
+}
